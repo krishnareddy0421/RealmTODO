7d4979f45225c07ab6c41cad1e8aca6d83908e79
vamsi krishna reddy kamjula <krishnareddy0421@gmail.com>
2017-11-26 12:00:48 -0500

    TextFields Added

--- RealmTODO/Controllers/AddVC.swift
+++ RealmTODO/Controllers/AddVC.swift
@@ -10,12 +10,32 @@ import UIKit
 
 class AddVC: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var titleTxt: UITextField!
+    @IBOutlet weak var timeTxt: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    @IBAction func saveBtnPressed(_ sender: Any) {
+        guard let title = titleTxt.text, let time = timeTxt.text, titleTxt.text != "", timeTxt.text != "" else {
+            showAlert("Start Typing")
+            return
+        }
+        
+        print("\(title), \(time)")
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func cancelBtnPressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func showAlert(_ titleTxt: String) {
+        let alert = UIAlertController.init(title: titleTxt, message: "Something went wrong", preferredStyle: .alert)
+        alert.addAction(UIAlertAction.init(title: "Ok", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
---
+++ RealmTODO/Model/Work.swift
@@ -0,0 +1,11 @@
+//
+//  Work.swift
+//  RealmTODO
+//
+//  Created by vamsi krishna reddy kamjula on 11/26/17.
+//  Copyright © 2017 kvkr. All rights reserved.
+//
+
+import Foundation
+
+
